a880e88a6b27874f69194d01be67716890f806bb
Augusto Costa <augustogiles@gmail.com>
2019-09-16 00:17:43 +0100

    adding list of wine in the screen

---
+++ src/components/Item/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { SRowItem, SFigure } from './styles';
+
+
+const Item = ( { wine, index, onClickForDetails } ) => {
+    console.log(index, onClickForDetails)
+
+    function handleClick(e){
+        e.preventDefault();
+        onClickForDetails(index);
+    }
+
+    return (
+        
+        <SRowItem>
+            <div className="img-box">
+                <SFigure image={wine.src}/>
+                {/* <img alt="wine-min" className="wine-min" src={wine.src}/> */}
+            </div>
+            <div className="wine-desc">
+                <div className="title"><a href={`/wine/${index}`} onClick={handleClick}>{wine.name}</a></div>
+                <div className="meta">
+                    <div className="block">
+                        <div className="block-desc">Average Rating</div>
+                        <div className="block-meta-data">
+                            <div className="data1">{wine.ratings_average.toFixed(1)}</div>
+                            <div className="data2">{wine.ratings_count} ratings</div>
+                        </div>
+                    </div>
+                    <div className="block">
+                        <div className="block-desc">Average Price</div>
+                        <div className="block-meta-data">
+                            <div className="data1">€ {wine.amount.toFixed(2)}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </SRowItem>
+    );
+}
+
+export default Item;
--- src/pages/Drinks/index.js
+++ src/pages/Drinks/index.js
@@ -3,25 +3,48 @@ import React from 'react'
 import { bindActionCreators, compose } from 'redux'
 import { connect } from 'react-redux'
 import { Creators as CreatorsDrinks } from '../../store/ducks/drinks'
+import { Content } from '../../components/Content/styles'
+import Item from '../../components/Item'
 
 
 class Drinks extends React.Component {
+    state = {
+        drinks: [],
+        sDrinks: []
+    }
+
+    componentDidMount(){
+        console.log(this.props.getDrinks())
+    }
+
+    componentDidUpdate(prevProps) {
+        prevProps.sDrinks !== this.props.sDrinks && this.handleUpdateDrinks()
+    }
+
+    handleUpdateDrinks = () => {
+        const { sDrinks } = this.props
+        const { drinks } = sDrinks
+        this.setState({ sDrinks, drinks })
+    }
+
+    handleDetails = (id) => {
+        console.log('click', id)
+    }
 
     render(){
+        const {drinks} = this.state
+        console.log(drinks.length)
         return (
-            <React.Fragment>
-                ALO
-            </React.Fragment>
+            <Content>
+                {drinks.map((drink, i) => <Item key={i} wine={drink} index={i} onClickForDetails={this.handleDetails} />)}
+            </Content>
         )
     }
 }
 
-const mapStateToProps = state => {
-    console.log(state)
-    return (
-{
-    sCards: state.home,
-})}
+const mapStateToProps = state => ({
+    sDrinks: state.drinks,
+})
 
 const mapDispatchToProps = dispatch => bindActionCreators(CreatorsDrinks, dispatch)
 
