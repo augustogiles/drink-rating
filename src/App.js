6492f9bcb0879fb31705dbe95523ae254476e22e
Augusto Costa <augustogiles@gmail.com>
2019-09-15 16:37:35 +0100

    adding header component in the root page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
+import Header from './components/Header'
 import Routes from './routes'
 
 class App extends React.Component {
@@ -8,6 +9,7 @@ class App extends React.Component {
     render(){
         return (
             <React.Fragment>
+                <Header/>
                 <Routes />
             </React.Fragment>
         )
